01d673c0d2de8e82ddf4378e2d31023fa68da612
童军华 <tongjunhua@tongjunhuadeMac-mini.local>
2020-03-21 17:19:26 +0800

    add

---
+++ src/store/modules/user.ts
@@ -0,0 +1,136 @@
+import { VuexModule, Module, Action, Mutation, getModule } from 'vuex-module-decorators'
+import { login, logout, getUserInfo } from '@/api/users'
+import { getToken, setToken, removeToken } from '@/utils/cookies'
+import router, { resetRouter } from '@/router'
+import { PermissionModule } from './permission'
+import { TagsViewModule } from './tags-view'
+import store from '@/store'
+import request from '@/utils/request'
+
+export interface IUserState {
+  token: string
+  name: string
+  avatar: string
+  introduction: string
+  roles: string[]
+  email: string,
+  session: Object
+}
+
+@Module({ dynamic: true, store, name: 'user' })
+class User extends VuexModule implements IUserState {
+  public token = getToken() || ''
+  public name = ''
+  public avatar = ''
+  public introduction = ''
+  public roles: string[] = []
+  public email = ''
+  public session = {}
+
+  @Mutation
+  private SET_TOKEN(token: string) {
+    this.token = token
+  }
+
+  @Mutation
+  private SET_NAME(name: string) {
+    this.name = name
+  }
+
+  @Mutation
+  private SET_AVATAR(avatar: string) {
+    this.avatar = avatar
+  }
+
+  @Mutation
+  private SET_INTRODUCTION(introduction: string) {
+    this.introduction = introduction
+  }
+
+  @Mutation
+  private SET_ROLES(roles: string[]) {
+    this.roles = roles
+  }
+
+  @Mutation
+  private SET_EMAIL(email: string) {
+    this.email = email
+  }
+
+  @Action
+  public async Login(userInfo: { username: string, password: string}) {
+    let { username, password } = userInfo
+    username = username.trim()
+    // const { data } = await login({ username, password })
+
+    const { data } = await request({
+      url: '/user/login',
+      method: 'post',
+      data: { username, password }
+    })
+    console.log( data )
+    this.session = data;
+    setToken(data.Authorization)
+    this.SET_TOKEN(data.Authorization)
+  }
+
+  @Action
+  public ResetToken() {
+    removeToken()
+    this.SET_TOKEN('')
+    this.SET_ROLES([])
+  }
+
+  @Action
+  public async GetUserInfo() {
+    if (this.token === '') {
+      throw Error('GetUserInfo: token is undefined!')
+    }
+    const { data } = await request({
+      url: '/user/info',
+      method: 'get'
+    })
+    console.log( data )
+    if (!data) {
+      throw Error('Verification failed, please Login again.')
+    }
+    this.SET_ROLES(data.roles)
+    this.SET_NAME(data.name)
+    this.SET_AVATAR(data.face)
+    this.SET_INTRODUCTION(data.description)
+    this.SET_EMAIL(data.email)
+  }
+
+  @Action
+  public async ChangeRoles(role: string) {
+    // Dynamically modify permissions
+    const token = role + '-token'
+    this.SET_TOKEN(token)
+    setToken(token)
+    await this.GetUserInfo()
+    resetRouter()
+    // Generate dynamic accessible routes based on roles
+    PermissionModule.GenerateRoutes(this.roles)
+    // Add generated routes
+    router.addRoutes(PermissionModule.dynamicRoutes)
+    // Reset visited views and cached views
+    TagsViewModule.delAllViews()
+  }
+
+  @Action
+  public async LogOut() {
+    if (this.token === '') {
+      throw Error('LogOut: token is undefined!')
+    }
+    await logout()
+    removeToken()
+    resetRouter()
+
+    // Reset visited views and cached views
+    TagsViewModule.delAllViews()
+    this.SET_TOKEN('')
+    this.SET_ROLES([])
+  }
+}
+
+export const UserModule = getModule(User)
---
+++ src/utils/request.ts
@@ -0,0 +1,90 @@
+import axios from 'axios'
+import { Message, MessageBox } from 'element-ui'
+import { UserModule } from '@/store/modules/user'
+import qs from "qs";
+
+const service = axios.create({
+  baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
+  timeout: 5000
+  // withCredentials: true // send cookies when cross-domain requests
+})
+
+// Request interceptors
+service.interceptors.request.use(
+  (config) => {
+    // Add X-Access-Token header to every request, you can add other custom headers here
+    if (UserModule.token) {
+      config.headers['Authorization'] = UserModule.token
+    }
+
+    if (config.method == 'post' || config.method == 'put') {
+      config.headers['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8' /// /解决axios提交post问题
+    }
+
+    // const params = new URLSearchParams()
+    // for (const key in config.data) {
+    //   if (config.data.hasOwnProperty(key)) {
+    //     params.append(key, config.data[key])
+    //   }
+    // }
+    // config.data = params
+    config.data = qs.stringify(config.data)
+
+    console.log( config )
+
+    return config
+  },
+  (error) => {
+    Promise.reject(error)
+  }
+)
+
+// Response interceptors
+service.interceptors.response.use(
+  (response) => {
+    // Some example codes here:
+    // code == 20000: success
+    // code == 50001: invalid access token
+    // code == 50002: already login in other place
+    // code == 50003: access token expired
+    // code == 50004: invalid user (user not exist)
+    // code == 50005: username or password is incorrect
+    // You can change this part for your own usage.
+    const res = response.data
+    console.log(res)
+    if (res.status !== 0) {
+      Message({
+        message: res.message || 'Error',
+        type: 'error',
+        duration: 5 * 1000
+      })
+      if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
+        MessageBox.confirm(
+          '你已被登出，可以取消继续留在该页面，或者重新登录',
+          '确定登出',
+          {
+            confirmButtonText: '重新登录',
+            cancelButtonText: '取消',
+            type: 'warning'
+          }
+        ).then(() => {
+          UserModule.ResetToken()
+          location.reload() // To prevent bugs from vue-router
+        })
+      }
+      return Promise.reject(new Error(res.message || 'Error'))
+    } else {
+      return response.data
+    }
+  },
+  (error) => {
+    // Message({
+    //   message: error.message,
+    //   type: 'error',
+    //   duration: 5 * 1000
+    // })
+    return Promise.reject(error)
+  }
+)
+
+export default service
